9c293186d0dab8a5c23160c52ab399f6f91c979d
MCLoebl <matthias.loebl@rwth-aachen.de>
2024-02-22 20:40:31 +0100

    analyse time scaling with size

--- py_wrapper/bench_decoder_analysis.py
+++ py_wrapper/bench_decoder_analysis.py
@@ -51,12 +51,47 @@ def num_decoding_failures_b(DecoderClass, H, logicals, p, num_trials):
 
 
 if __name__ == "__main__":
-    num_trials = 5000
-    l_decoder = ['uf2', 'matching', 'uf']
-    l_L = [10, 20, 40, 80]
-    l_p = [0.006*i for i in range(20)]
     l_col1 = ['-r', '-g', '-b']
     l_col2 = [':r', ':g', ':b']
+    l_decoder = ['uf2', 'matching', 'uf']
+
+    # speed scaling with size
+    num_trials = 3000
+    l_L = [10*(i+1) for i in range(14)]
+    l_p = [0.02, 0.04, 0.06]
+    fig1, ax1 = plt.subplots()
+    a_time = np.zeros((len(l_decoder), len(l_L), len(l_p)), dtype=np.double)
+    for i_dec, dec in enumerate(l_decoder):
+        print(dec)
+        if dec == 'uf':
+            DecoderClass = UnionFindPy.Decoder
+        elif dec == 'matching':
+            DecoderClass = pymatching.Matching
+        elif dec == 'uf2':
+            DecoderClass = TannerGraphDecoder
+        for i_L, L in enumerate(l_L):
+            print(L)
+            Hx = toric_code_x_stabilisers(L)
+            logX = toric_code_x_logicals(L)
+            for i_p, p in enumerate(l_p):
+                if dec == 'uf2':
+                    total_time, num_errors = num_decoding_failures_b(DecoderClass, Hx, logX, p, num_trials)
+                elif dec == 'matching':
+                    total_time, num_errors = num_decoding_failures(DecoderClass, Hx, logX, p, num_trials)
+                elif dec == 'uf':  # https://github.com/chaeyeunpark/UnionFind needs csr_matrix
+                    total_time, num_errors = num_decoding_failures(DecoderClass, csr_matrix(Hx), logX, p, num_trials)
+                a_time[i_dec, i_L, i_p] = total_time / num_trials
+        for i_p, _ in enumerate(l_p):
+            ax1.loglog(np.array(l_L)**2, a_time[i_dec, :, i_p], l_col1[i_dec])
+    ax1.loglog(np.array([l_L[0]**2, l_L[-1]**2]), 10**-7 * np.array([l_L[0]**2, l_L[-1]**2]), '--k')  # linear reference
+    ax1.set_xlabel('size')
+    ax1.set_ylabel('time')
+    plt.savefig('time_scaling.pdf')
+
+    # comparison of threhsold and speed (different sizes, different error rates)
+    num_trials = 5000
+    l_L = [10, 20, 40, 80]
+    l_p = [0.006*i for i in range(20)]
     fig1, ax1 = plt.subplots()
     ax2 = ax1.twinx()
     for i_dec, dec in enumerate(l_decoder):
