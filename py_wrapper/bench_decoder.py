ad32a3f4bb2db13c29504e2e1e5c63b99310a6f8
MCLoebl <matthias.loebl@rwth-aachen.de>
2024-03-28 11:54:44 +0100

    batch evaluation in pymatching, minor stuff

--- py_wrapper/bench_decoder.py
+++ py_wrapper/bench_decoder.py
@@ -1,6 +1,3 @@
-# This code is based on PyMatching document: https://pymatching.readthedocs.io/en/stable/toric-code-example.html
-# Modified for the C++ UnionFind Project <https://github.com/chaeyeunpark/UnionFind>, and https://github.com/nbi-hyq/speed_decoder
-
 import numpy as np
 import matplotlib.pyplot as plt
 import UnionFindPy
@@ -8,80 +5,35 @@ import pymatching
 import sys
 import time
 from run_from_py import toric_code_x_logicals, toric_code_x_stabilisers, TannerGraphDecoder
+from bench_decoder_analysis import num_decoding_failures_batch, num_decoding_failures_pymatching_batch, num_decoding_failures_others
 from scipy.sparse import csr_matrix
 
 
-def num_decoding_failures(DecoderClass, H, logicals, p, num_trials):
-    decoder = DecoderClass(H)
-    num_errors = 0
-    total_time = 0.0
-    for i in range(num_trials):
-        noise = np.random.binomial(1, p, 2 * L * L)
-        syndrome = H @ noise % 2
+DEFAULT_NUM_TRIALS = 10000
 
-        start_time = time.perf_counter()
-        correction = decoder.decode(syndrome)
-        end_time = time.perf_counter()
-        
-        total_time += (end_time - start_time)
-        error = (noise + correction) % 2
-        if np.any(error @ logicals.T % 2):
-            num_errors += 1
-    return total_time, num_errors
+if len(sys.argv) not in [4, 5]:
+    print("Usage: {} [uf | matching] L p [num_trials]".format(sys.argv[0]))
+    print(f'   Default value for num_trials is {DEFAULT_NUM_TRIALS}')
 
+if sys.argv[1].strip().lower() not in ['uf', 'matching', 'uf_reference']:
+    print("The first argument must be 'uf', 'matching', 'uf_reference'")
+    sys.exit(1)
 
-def num_decoding_failures_b(DecoderClass, H, logicals, p, num_trials):
-    decoder = DecoderClass(H)
-    num_errors = 0
-    total_time = 0.0
-    for i in range(num_trials):
-        noise = np.random.binomial(1, p, 2 * L * L)
-        syndrome = np.zeros(decoder.nnode, dtype=np.uint8)
-        syndrome[:decoder.nsyndromes] = H @ noise % 2
+L = int(sys.argv[2])
+p = float(sys.argv[3])
 
-        start_time = time.perf_counter()
-        decoder.decode(syndrome)
-        end_time = time.perf_counter()
-        
-        total_time += (end_time - start_time)
-        error = (noise + decoder.correction[decoder.nsyndromes:]) % 2
-        if np.any(error @ logicals.T % 2):
-            num_errors += 1
-    return total_time, num_errors
+if len(sys.argv) == 5:
+    num_trials = int(sys.argv[4])
+else:
+    num_trials = DEFAULT_NUM_TRIALS
 
+Hx = toric_code_x_stabilisers(L)
+logX = toric_code_x_logicals(L)
+if sys.argv[1].strip().lower() == 'uf':
+    total_time, num_errors = num_decoding_failures_batch(Hx, logX, p, num_trials)
+elif sys.argv[1].strip().lower() == 'matching':
+    total_time, num_errors = num_decoding_failures_pymatching_batch(Hx, logX, p, num_trials)
+elif sys.argv[1].strip().lower() == 'uf_reference':  # https://github.com/chaeyeunpark/UnionFind needs csr_matrix
+    total_time, num_errors = num_decoding_failures_others(UnionFindPy.Decoder, csr_matrix(Hx), logX, p, num_trials)
 
-if __name__ == "__main__":
-    DEFAULT_NUM_TRIALS = 10000
-
-    if len(sys.argv) not in [4, 5]:
-        print("Usage: {} [uf | matching] L p [num_trials]".format(sys.argv[0]))
-        print(f'   Default value for num_trials is {DEFAULT_NUM_TRIALS}')
-
-    if sys.argv[1].strip().lower() == 'uf':
-        DecoderClass = UnionFindPy.Decoder
-    elif sys.argv[1].strip().lower() == 'matching':
-        DecoderClass = pymatching.Matching
-    elif sys.argv[1].strip().lower() == 'uf2':
-        DecoderClass = TannerGraphDecoder
-    else:
-        print("The first argument must be 'uf', 'matching', or 'uf2'")
-        sys.exit(1)
-
-    L = int(sys.argv[2])
-    p = float(sys.argv[3])
-
-    if len(sys.argv) == 5:
-        num_trials = int(sys.argv[4])
-    else:
-        num_trials = DEFAULT_NUM_TRIALS
-    
-    Hx = toric_code_x_stabilisers(L)
-    logX = toric_code_x_logicals(L)
-    if sys.argv[1].strip().lower() == 'uf2':
-        total_time, num_errors = num_decoding_failures_b(DecoderClass, Hx, logX, p, num_trials)
-    elif sys.argv[1].strip().lower() == 'matching':
-        total_time, num_errors = num_decoding_failures(DecoderClass, Hx, logX, p, num_trials)
-    elif sys.argv[1].strip().lower() == 'uf':  # https://github.com/chaeyeunpark/UnionFind needs csr_matrix
-        total_time, num_errors = num_decoding_failures(DecoderClass, csr_matrix(Hx), logX, p, num_trials)
-
-    print('{}\t{}'.format(total_time / num_trials, num_errors / num_trials))
+print('{}\t{}'.format(total_time / num_trials, num_errors / num_trials))
--- py_wrapper/bench_decoder_analysis.py
+++ py_wrapper/bench_decoder_analysis.py
@@ -1,6 +1,3 @@
-# This code is based on PyMatching document: https://pymatching.readthedocs.io/en/stable/toric-code-example.html
-# Modified for the C++ UnionFind Project <https://github.com/chaeyeunpark/UnionFind>, and https://github.com/nbi-hyq/speed_decoder
-
 import numpy as np
 import matplotlib.pyplot as plt
 import UnionFindPy
@@ -12,55 +9,35 @@ from scipy.sparse import csr_matrix
 
 
 # call library only once for block of many repetitions
-def num_decoding_failures_batch(DecoderClass, H, logicals, p, num_trials):
-    decoder = DecoderClass(H)
-    decoder.correction = np.zeros(decoder.nnode * num_trials, dtype=np.uint8)
-    syndrome = np.zeros(decoder.nnode * num_trials, dtype=np.uint8)
+def num_decoding_failures_batch(H, logicals, p_err, num_rep):
+    decoder = TannerGraphDecoder(H)
+    decoder.correction = np.zeros(decoder.nnode * num_rep, dtype=np.uint8)
+    syndrome = np.zeros(decoder.nnode * num_rep, dtype=np.uint8)
     l_noise = []
-    for i in range(num_trials):
-        l_noise.append(np.random.binomial(1, p, H.shape[1]))
+    for i in range(num_rep):
+        l_noise.append(np.random.binomial(1, p_err, H.shape[1]))
         syndrome[i*decoder.nnode:i*decoder.nnode+decoder.nsyndromes] = H @ l_noise[i] % 2
 
     start_time = time.perf_counter()
-    decoder.decode_batch(syndrome, num_trials)
+    decoder.decode_batch(syndrome, num_rep)
     end_time = time.perf_counter()
-    total_time = end_time - start_time
+    time_decode = end_time - start_time
 
-    num_errors = 0
-    for i in range(num_trials):
+    n_err = 0
+    for i in range(num_rep):
         error = (l_noise[i] + decoder.correction[i*decoder.nnode+decoder.nsyndromes:(i+1)*decoder.nnode]) % 2
         if np.any(error @ logicals.T % 2):
-            num_errors += 1
-    return total_time, num_errors
+            n_err += 1
+    return time_decode, n_err
 
 
-# call library for every repetition
-def num_decoding_failures(DecoderClass, H, logicals, p, num_trials):
-    decoder = DecoderClass(H)
-    num_errors = 0
-    total_time = 0.0
-    for i in range(num_trials):
-        noise = np.random.binomial(1, p, H.shape[1])
-        syndrome = H @ noise % 2
-
-        start_time = time.perf_counter()
-        correction = decoder.decode(syndrome)
-        end_time = time.perf_counter()
-        
-        total_time += (end_time - start_time)
-        error = (noise + correction) % 2
-        if np.any(error @ logicals.T % 2):
-            num_errors += 1
-    return total_time, num_errors
-
-
-# call library for every repetition
-def num_decoding_failures_b(DecoderClass, H, logicals, p, num_trials):
-    decoder = DecoderClass(H)
-    num_errors = 0
-    total_time = 0.0
-    for i in range(num_trials):
-        noise = np.random.binomial(1, p, H.shape[1])
+# call library for every repetition (correction is written into existing array decoder.correction)
+def num_decoding_failures(H, logicals, p_err, num_rep):
+    decoder = TannerGraphDecoder(H)
+    n_err = 0
+    time_decode = 0.0
+    for i in range(num_rep):
+        noise = np.random.binomial(1, p_err, H.shape[1])
         syndrome = np.zeros(decoder.nnode, dtype=np.uint8)
         syndrome[:decoder.nsyndromes] = H @ noise % 2
 
@@ -68,47 +45,78 @@ def num_decoding_failures_b(DecoderClass, H, logicals, p, num_trials):
         decoder.decode(syndrome)
         end_time = time.perf_counter()
         
-        total_time += (end_time - start_time)
+        time_decode += (end_time - start_time)
         error = (noise + decoder.correction[decoder.nsyndromes:]) % 2
         if np.any(error @ logicals.T % 2):
-            num_errors += 1
-    return total_time, num_errors
+            n_err += 1
+    return time_decode, n_err
+
+
+# call library for every repetition (for other decoders that return correction as new array)
+def num_decoding_failures_others(decoder_used, H, logicals, p_err, num_rep):
+    decoder = decoder_used(H)
+    n_err = 0
+    time_decode = 0.0
+    for i in range(num_rep):
+        noise = np.random.binomial(1, p_err, H.shape[1])
+        syndrome = H @ noise % 2
+
+        start_time = time.perf_counter()
+        correction = decoder.decode(syndrome)
+        end_time = time.perf_counter()
+        
+        time_decode += (end_time - start_time)
+        error = (noise + correction) % 2
+        if np.any(error @ logicals.T % 2):
+            n_err += 1
+    return time_decode, n_err
+
+
+# for pymatching: call library only once for block of many repetitions
+def num_decoding_failures_pymatching_batch(H, logicals, p_err, num_rep):
+    decoder = pymatching.Matching.from_check_matrix(H, faults_matrix=logicals)
+    noise = (np.random.random((num_rep, H.shape[1])) < p_err).astype(np.uint8)
+    shots = (noise @ H.T) % 2
+    actual_observables = (noise @ logicals.T) % 2
+    start_time = time.perf_counter()
+    predicted_observables = decoder.decode_batch(shots)
+    end_time = time.perf_counter()
+    time_decode = end_time - start_time
+    n_err = np.sum(np.any(predicted_observables != actual_observables, axis=1))
+    return time_decode, n_err
 
 
 if __name__ == "__main__":
     l_col1 = ['-r', '-g', '-b']
     l_col2 = [':r', ':g', ':b']
-    l_decoder = ['uf2', 'matching', 'uf']
-    batch_evaluate = True  # do error sampling in C-progrm and do all repetitions of same parameter in one block (only used for uf2-decoder)
+    l_decoder = ['uf', 'matching', 'uf_reference']
+    batch_evaluate = True  # do all repetitions of same parameter in one block
 
     # speed scaling with size
-    num_trials = 3000
-    l_L = [10*(i+1) for i in range(14)]
-    l_p = [0.02, 0.04, 0.06, 0.08]
+    num_trials = 300
+    l_L = [10*(i+1) for i in range(16)]
+    l_p = [0.01, 0.05, 0.09]
     fig1, ax1 = plt.subplots()
     a_time = np.zeros((len(l_decoder), len(l_L), len(l_p)), dtype=np.double)
     for i_dec, dec in enumerate(l_decoder):
         print(dec)
-        if dec == 'uf':
-            DecoderClass = UnionFindPy.Decoder
-        elif dec == 'matching':
-            DecoderClass = pymatching.Matching
-        elif dec == 'uf2':
-            DecoderClass = TannerGraphDecoder
         for i_L, L in enumerate(l_L):
             print(L)
             Hx = toric_code_x_stabilisers(L)
             logX = toric_code_x_logicals(L)
             for i_p, p in enumerate(l_p):
-                if dec == 'uf2':
+                if dec == 'uf':
                     if batch_evaluate:
-                        total_time, num_errors = num_decoding_failures_batch(DecoderClass, Hx, logX, p, num_trials)
+                        total_time, num_errors = num_decoding_failures_batch(Hx, logX, p, num_trials)
                     else:
-                        total_time, num_errors = num_decoding_failures_b(DecoderClass, Hx, logX, p, num_trials)
+                        total_time, num_errors = num_decoding_failures(Hx, logX, p, num_trials)
                 elif dec == 'matching':
-                    total_time, num_errors = num_decoding_failures(DecoderClass, Hx, logX, p, num_trials)
-                elif dec == 'uf':  # https://github.com/chaeyeunpark/UnionFind needs csr_matrix
-                    total_time, num_errors = num_decoding_failures(DecoderClass, csr_matrix(Hx), logX, p, num_trials)
+                    if batch_evaluate:
+                        total_time, num_errors = num_decoding_failures_pymatching_batch(Hx, logX, p, num_trials)
+                    else:
+                        total_time, num_errors = num_decoding_failures_others(pymatching.Matching.from_check_matrix, Hx, logX, p, num_trials)
+                elif dec == 'uf_reference':  # https://github.com/chaeyeunpark/UnionFind needs csr_matrix
+                    total_time, num_errors = num_decoding_failures_others(UnionFindPy.Decoder, csr_matrix(Hx), logX, p, num_trials)
                 a_time[i_dec, i_L, i_p] = total_time / num_trials
         for i_p, _ in enumerate(l_p):
             ax1.loglog(np.array(l_L)**2, a_time[i_dec, :, i_p], l_col1[i_dec])
@@ -117,20 +125,14 @@ if __name__ == "__main__":
     ax1.set_ylabel('time')
     plt.savefig('time_scaling.pdf')
 
-    # comparison of threhsold and speed (different sizes, different error rates)
-    num_trials = 5000
+    # comparison of threshold and speed (different sizes, different error rates)
+    num_trials = 500
     l_L = [10, 20, 30, 40]
     l_p = [0.006*i for i in range(20)]
     fig1, ax1 = plt.subplots()
     ax2 = ax1.twinx()
     for i_dec, dec in enumerate(l_decoder):
         print(dec)
-        if dec == 'uf':
-            DecoderClass = UnionFindPy.Decoder
-        elif dec == 'matching':
-            DecoderClass = pymatching.Matching
-        elif dec == 'uf2':
-            DecoderClass = TannerGraphDecoder
         for L in l_L:
             print(L)
             Hx = toric_code_x_stabilisers(L)
@@ -138,15 +140,18 @@ if __name__ == "__main__":
             l_time = []
             l_error = []
             for p in l_p:
-                if dec == 'uf2':
+                if dec == 'uf':
                     if batch_evaluate:
-                        total_time, num_errors = num_decoding_failures_batch(DecoderClass, Hx, logX, p, num_trials)
+                        total_time, num_errors = num_decoding_failures_batch(Hx, logX, p, num_trials)
                     else:
-                        total_time, num_errors = num_decoding_failures_b(DecoderClass, Hx, logX, p, num_trials)
+                        total_time, num_errors = num_decoding_failures(Hx, logX, p, num_trials)
                 elif dec == 'matching':
-                    total_time, num_errors = num_decoding_failures(DecoderClass, Hx, logX, p, num_trials)
-                elif dec == 'uf':  # https://github.com/chaeyeunpark/UnionFind needs csr_matrix
-                    total_time, num_errors = num_decoding_failures(DecoderClass, csr_matrix(Hx), logX, p, num_trials)
+                    if batch_evaluate:
+                        total_time, num_errors = num_decoding_failures_pymatching_batch(Hx, logX, p, num_trials)
+                    else:
+                        total_time, num_errors = num_decoding_failures_others(pymatching.Matching.from_check_matrix, Hx, logX, p, num_trials)
+                elif dec == 'uf_reference':  # https://github.com/chaeyeunpark/UnionFind needs csr_matrix
+                    total_time, num_errors = num_decoding_failures_others(UnionFindPy.Decoder, csr_matrix(Hx), logX, p, num_trials)
                 l_error.append(num_errors / num_trials)
                 l_time.append(total_time / num_trials)
             ax1.semilogy(l_p, l_error, l_col1[i_dec])
@@ -155,4 +160,3 @@ if __name__ == "__main__":
     ax1.set_ylabel('logical error rate')
     ax2.set_ylabel('time (s)')
     plt.savefig('error_and_time.pdf')
-            
